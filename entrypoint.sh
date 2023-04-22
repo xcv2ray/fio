e889198b26483d2cf315bbab565a4b9b53f2376d
xcv2ray <128805561+xcv2ray@users.noreply.github.com>
2023-04-22 08:55:19 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 设置各变量
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
+UUID='917cd086-5695-4302-8dcb-e843b0ad89db'
 VMESS_WSPATH='/vmess'
 VLESS_WSPATH='/vless'
 TROJAN_WSPATH='/trojan'
